4975d048242c507bc947e7f49a12f72e29f3a945
Gajol <doug@nielsenfamily.ca>
2021-05-08 08:24:41 -0400

    Adding react-app - maybe too many files

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Contacts from './components/contacts';
+
+class App extends Component {
+
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ contacts: data })
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    return (
+      <Contacts contacts={this.state.contacts} />
+    )
+  }
+  
+}
+
+/*
+
+class App extends Component {
+  render() {
+    return (
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Steve Jobs</h5>
+          <h6 class="card-subtitle mb-2 text-muted">steve@apple.com</h6>
+          <p class="card-text">Stay Hungry, Stay Foolish</p>
+        </div>
+      </div>
+    );
+  }
+}
+*/
+
+export default App;
